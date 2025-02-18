57faa3099e210ebf386b0afe00f27cc4bb27b6bd
rond3vs <rond3vs@gmail.com>
2025-02-18 14:02:45 -0600

    solved test 7.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+// (0°C × 9/5) + 32 = 32°F - to F
+// (32°F − 32) × 5/9 = 0°C - to C
+
+
+const convertToCelsius = function(temp) {
+  if (typeof temp !== 'number') return 'Error: Input must be a number';
+  let celsiusTemp = (temp - 32) * (5 / 9);
+  return Number(celsiusTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  if (typeof temp !== 'number') return 'Error: Input must be a number';
+  let freedomUnits = (temp * (9 / 5)) + 32;
+  return Number(freedomUnits.toFixed(1));
 };
 
 // Do not edit below this line
