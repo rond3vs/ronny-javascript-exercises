dd32357acec6217904818be2ad47644778f272db
rond3vs <rond3vs@gmail.com>
2025-02-18 13:29:29 -0600

    solved #5 test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (typeof x !== 'number' ||
+        typeof y !== 'number' ||
+        x < 0 || 
+        y < 0 || 
+        !Number.isInteger(x) || 
+        !Number.isInteger(y)
+    ){
+        return 'ERROR'
+    }
 
-};
+    const min = Math.min(x,y);
+    const max = Math.max(x,y);
+
+    const sum = ((min+max)*((max-min+1))/2);
+
+    return sum;
+}
+
+console.log(sumAll(6,10))
+    
 
 // Do not edit below this line
 module.exports = sumAll;
